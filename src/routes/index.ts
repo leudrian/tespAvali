188693ee30a24623b65ce36c6b917e53a25fa476
leudrian <emanueladrian36@gmail.com>
2022-10-26 19:49:16 -0400

    merge

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import UserController from '../controllers/UserControllers';
 const routes = express.Router();
 
-routes.get('/listar', UserControllers.listar);
+routes.get('/listar', UserController.listar);
 // continuar aqui
-routes.get('/usuario/:id', UserControllers.listarId)
-routes.post('/usuario/idade', UserControllers.buscarIdade)
-routes.post('/usuario/sexo', UserControllers.buscarSexo)
+routes.get('/usuario/:id', UserController.buscarId)
+routes.post('/usuario/idade', UserController.buscarIdade)
+routes.post('/usuario/sexo', UserController.buscarSexo)
 export default routes;
